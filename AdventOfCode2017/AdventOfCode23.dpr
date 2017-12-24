669d2e54e782008f2c228f257bd823c27a2c05b8
Primoz Gabrijelcic <primoz@gabrijelcic.org>
2017-12-24 16:34:32 +0100

    Advent of code, day 23, part A

---
+++ AdventOfCode2017/AdventOfCode23.dpr
@@ -0,0 +1,122 @@
+program AdventOfCode23;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils, System.Generics.Collections,
+  GpStreams, GpTextStream;
+
+type
+  TInstruction = record
+    Inst: string;
+    Reg : string;
+    Val : string;
+  end;
+
+  TCPU = class
+  strict private
+    FRecovered: integer;
+    FSound    : int64;
+  strict protected
+    FIP       : integer;
+    FMulCount : integer;
+    FProgram  : TList<TInstruction>;
+    FRegisters: array ['a'..'z'] of int64;
+    procedure Execute(const instr: TInstruction); virtual;
+    function Value(const regVal: string): int64;
+  public
+    constructor Create(const fileName: string);
+    destructor Destroy; override;
+    function Run: int64;
+  end;
+
+function PartA(const fileName: string): integer;
+var
+  cpu: TCPU;
+begin
+  cpu := TCPU.Create(fileName);
+  try
+    Result := cpu.Run;
+  finally FreeAndNil(cpu); end;
+end;
+
+{ TCPU }
+
+constructor TCPU.Create(const fileName: string);
+var
+  instr: TInstruction;
+  line : string;
+  parts: TArray<string>;
+begin
+  inherited Create;
+  FProgram := TList<TInstruction>.Create;
+  for line in EnumLines(AutoDestroyStream(SafeCreateFileStream(fileName, fmOpenRead)).Stream) do
+  begin
+    parts := line.Split([' ']);
+    instr.Inst := parts[0];
+    instr.Reg := parts[1];
+    if High(parts) > 1 then
+      instr.Val := parts[2]
+    else
+      instr.Val := '';
+    FProgram.Add(instr);
+  end;
+end;
+
+destructor TCPU.Destroy;
+begin
+  FreeAndNil(FProgram);
+  inherited;
+end;
+
+procedure TCPU.Execute(const instr: TInstruction);
+begin
+  if instr.Inst = 'set' then
+    FRegisters[instr.Reg[1]] := Value(instr.Val)
+  else if instr.Inst = 'sub' then
+    FRegisters[instr.Reg[1]] := FRegisters[instr.Reg[1]] - Value(instr.Val)
+  else if instr.Inst = 'mul' then begin
+    FRegisters[instr.Reg[1]] := FRegisters[instr.Reg[1]] * Value(instr.Val);
+    Inc(FMulCount);
+  end
+  else if instr.Inst = 'jnz' then begin
+    if Value(instr.Reg) <> 0 then
+      FIP := FIP + Value(instr.Val) - 1 {compensate};
+  end
+  else
+    raise Exception.Create('Invalid instruction ' + instr.Inst);
+  Inc(FIP);
+end;
+
+function TCPU.Run: int64;
+var
+  instr: TInstruction;
+begin
+  FIP := 0;
+  FillChar(FRegisters, SizeOf(FRegisters), 0);
+  repeat
+    instr := FProgram[FIP];
+    Execute(instr);
+  until (FIP < 0) or (FIP >= FProgram.Count);
+  Result := FMulCount;
+end;
+
+function TCPU.Value(const regVal: string): int64;
+begin
+  if regVal[1] in ['a'..'z'] then
+    Result := FRegisters[regVal[1]]
+  else
+    Result := StrToInt64(regVal);
+end;
+
+begin
+  try
+    Writeln(PartA('..\..\AdventOfCode23.txt'));
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+  Readln;
+end.
